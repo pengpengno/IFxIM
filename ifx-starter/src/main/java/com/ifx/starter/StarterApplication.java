82172a323511d2fb2a629cc165635d9e6fd0fc0f
Allen.Pen <pengpeng_on@163.com>
2022-07-30 11:50:26 +0800

    fix maven conflict

--- ifx-starter/src/main/java/com/ifx/starter/StarterApplication.java
+++ ifx-starter/src/main/java/com/ifx/starter/StarterApplication.java
@@ -1,10 +1,11 @@
 package com.ifx.starter;
 
+import org.apache.dubbo.spring.boot.autoconfigure.DubboAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DubboAutoConfiguration.class})
 public class StarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class);
