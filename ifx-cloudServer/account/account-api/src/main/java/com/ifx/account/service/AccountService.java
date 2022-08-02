3a07c58829eb7db54c6d0836609fcc9bfda75427
付林 <1649737557@qq.com>
2022-08-02 20:49:46 +0800

    feat: 新增用户注册与登录功能

--- ifx-cloudServer/account/account-api/src/main/java/com/ifx/account/service/AccountService.java
+++ ifx-cloudServer/account/account-api/src/main/java/com/ifx/account/service/AccountService.java
@@ -1,7 +1,8 @@
 package com.ifx.account.service;
 
-import com.ifx.account.entity.Account;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.ifx.account.entity.Account;
+import com.ifx.account.vo.AccountBaseInfo;
 
 /**
 * @author wangpeng
@@ -9,7 +10,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2022-07-30 16:21:21
 */
 public interface AccountService extends IService<Account> {
-        public Boolean login();
+        public Boolean login(AccountBaseInfo accountBaseInfo);
+        public String register(AccountBaseInfo accountBaseInfo);
 
 //        public  pullFriReleation
 }
