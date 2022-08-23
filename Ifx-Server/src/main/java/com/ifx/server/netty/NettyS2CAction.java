400d6c48394b4b44d31d6e4a6fa7bd78cc81a1d0
pegnpeng <pengpeng_on@163.com>
2022-08-24 07:52:22 +0800

    sync

--- Ifx-Server/src/main/java/com/ifx/server/netty/NettyS2CAction.java
+++ Ifx-Server/src/main/java/com/ifx/server/netty/NettyS2CAction.java
@@ -1,24 +1,31 @@
 package com.ifx.server.netty;
 
+import com.alibaba.fastjson2.JSON;
 import com.ifx.connect.proto.Protocol;
 import com.ifx.server.netty.holder.NettyContext;
 import com.ifx.server.s2c.IServer2ClientAction;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-@Resource
+@Component
+@Slf4j
 public class NettyS2CAction implements IServer2ClientAction {
     @Resource
     private NettyContext nettyContext;
 
     @Override
-    public void sendProtoCol(Protocol protocol) {
-
+    public void sendProtoCol(Channel channel ,Protocol protocol) {
+        if (channel== null || !channel.isActive()){
+            log.warn("channel is close can not send message");
+          return;
+        }
+         channel.writeAndFlush(Unpooled.copiedBuffer(JSON.toJSONString(protocol), CharsetUtil.UTF_8));
     }
 
-    public void sendProtoCol(Channel ctx,Protocol protocol){
-//        ctx.writeAndFlush()
-    }
 
     protected void sendProtocol(String account , Protocol protocol){
         sendProtoCol(findChannel(account),protocol);
@@ -33,7 +40,8 @@ public class NettyS2CAction implements IServer2ClientAction {
     }
 
     @Override
-    public void closeChanel() {
-
+    public void closeChanel(Channel channel) {
+        log.info("正在关闭 channel {} ",channel.remoteAddress());
+        channel.close();
     }
 }
